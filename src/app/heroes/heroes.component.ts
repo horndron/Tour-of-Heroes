e05edb8fcf017c6b2999625288e9516ccdf5dce6
horndron <horndron@yandex.ru>
2022-03-06 21:21:46 +0300

    feat: add dashboard and routing

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -12,17 +12,10 @@ export class HeroesComponent implements OnInit {
 
   heroes: Hero[] = [];
 
-  selectedHero?: Hero;
-
   constructor(private heroService: HeroService, public messageService: MessageService) { }
 
   ngOnInit(): void {
     this.getHeroes();
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`Heroes component: Selected hero id=${hero.id}`);
   }
 
   getHeroes(): void {
